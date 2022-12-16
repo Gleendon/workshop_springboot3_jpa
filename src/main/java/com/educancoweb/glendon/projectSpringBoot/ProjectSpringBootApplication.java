3ea7fea19cbf98e8bddcb901fdaa2d239ac9dd04
Glendon Souza <102700138+Gleendon@users.noreply.github.com>
2022-12-16 14:16:09 -0300

    Project created

---
+++ src/main/java/com/educancoweb/glendon/projectSpringBoot/ProjectSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.educancoweb.glendon.projectSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringBootApplication.class, args);
+	
+	System.out.println("teste");
+	}
+
+}
