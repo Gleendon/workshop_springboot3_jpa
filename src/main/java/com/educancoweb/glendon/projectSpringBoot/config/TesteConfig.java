935622261134dd881854d532b6718ec1612cfd0a
Glendon Souza <102700138+Gleendon@users.noreply.github.com>
2022-12-20 10:29:01 -0300

    OrderStatus enum

--- src/main/java/com/educancoweb/glendon/projectSpringBoot/config/TesteConfig.java
+++ src/main/java/com/educancoweb/glendon/projectSpringBoot/config/TesteConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.educancoweb.glendon.projectSpringBoot.entities.Order;
 import com.educancoweb.glendon.projectSpringBoot.entities.User;
+import com.educancoweb.glendon.projectSpringBoot.entities.enums.OrderStatus;
 import com.educancoweb.glendon.projectSpringBoot.repositeries.OrderRepository;
 import com.educancoweb.glendon.projectSpringBoot.repositeries.UserRepository;
 
@@ -38,9 +39,9 @@ public class TesteConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");		
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
