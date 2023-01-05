81cbdb63430d65cd012790282d34d813a7693473
Glendon Souza <102700138+Gleendon@users.noreply.github.com>
2023-01-05 19:39:36 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educancoweb/glendon/projectSpringBoot/config/TesteConfig.java
+++ src/main/java/com/educancoweb/glendon/projectSpringBoot/config/TesteConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.educancoweb.glendon.projectSpringBoot.entities.Category;
 import com.educancoweb.glendon.projectSpringBoot.entities.Order;
+import com.educancoweb.glendon.projectSpringBoot.entities.OrderItem;
 import com.educancoweb.glendon.projectSpringBoot.entities.Product;
 import com.educancoweb.glendon.projectSpringBoot.entities.User;
 import com.educancoweb.glendon.projectSpringBoot.entities.enums.OrderStatus;
 import com.educancoweb.glendon.projectSpringBoot.repositeries.CategoryRepository;
+import com.educancoweb.glendon.projectSpringBoot.repositeries.OrderItemRepository;
 import com.educancoweb.glendon.projectSpringBoot.repositeries.OrderRepository;
 import com.educancoweb.glendon.projectSpringBoot.repositeries.ProductRepository;
 import com.educancoweb.glendon.projectSpringBoot.repositeries.UserRepository;
@@ -40,6 +42,9 @@ public class TesteConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	/* Implementando o CommandLine esta classe será executada
 	 * quando o programa for iniciado
@@ -66,7 +71,6 @@ public class TesteConfig implements CommandLineRunner {
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
 		
-		//productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");		
@@ -79,6 +83,13 @@ public class TesteConfig implements CommandLineRunner {
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 	
 }
---
+++ src/main/java/com/educancoweb/glendon/projectSpringBoot/entities/pk/OrderItemPK.java
@@ -0,0 +1,65 @@
+package com.educancoweb.glendon.projectSpringBoot.entities.pk;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.educancoweb.glendon.projectSpringBoot.entities.Order;
+import com.educancoweb.glendon.projectSpringBoot.entities.Product;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+/*
+ * Classe de assoiação entre as classes produto e pedido, esta classe possui atributos extras
+ * que não pertencem a nenhuma das dauas classes. 
+ * O ID desta classe é a composta pelo ID das duas classes produto e pedido 
+ */
+
+//Classe auxilar para ser uma chave primária composta entre produtos e pedido
+@Embeddable
+public class OrderItemPK implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+	
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(order, product);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPK other = (OrderItemPK) obj;
+		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+	}
+	
+	
+	
+	
+	
+}
