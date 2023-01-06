748063d896dc7ef2920c7896a09fef042daca604
Glendon Souza <102700138+Gleendon@users.noreply.github.com>
2023-01-06 18:43:51 -0300

    Subtotal & Total methods

--- src/main/java/com/educancoweb/glendon/projectSpringBoot/entities/Order.java
+++ src/main/java/com/educancoweb/glendon/projectSpringBoot/entities/Order.java
@@ -99,6 +99,16 @@ public class Order implements Serializable {
 	public void setPaymnet(Payment paymnet) {
 		this.paymnet = paymnet;
 	}
+	
+	public double getTotal() {
+		double sum = 0.0;
+		
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
