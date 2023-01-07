89100a120e292c70d8b3cd6259b6c4a60c1c783f
Glendon Souza <102700138+Gleendon@users.noreply.github.com>
2023-01-06 21:31:46 -0300

    Exception handling - delete

--- src/main/java/com/educancoweb/glendon/projectSpringBoot/services/UserService.java
+++ src/main/java/com/educancoweb/glendon/projectSpringBoot/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educancoweb.glendon.projectSpringBoot.entities.User;
 import com.educancoweb.glendon.projectSpringBoot.repositeries.UserRepository;
+import com.educancoweb.glendon.projectSpringBoot.services.exceptions.DatabaseException;
 import com.educancoweb.glendon.projectSpringBoot.services.exceptions.ResourceNotFoundException;
 
 
@@ -37,7 +40,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {			
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User upadate(Long id, User obj) {
